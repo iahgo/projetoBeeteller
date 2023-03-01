06f0f68b5ac84268a377a22a1f3f18c9d73effb1
iahgo <iahgo@icloud.com>
2023-03-01 11:29:21 -0300

    chore: deleted unused files from users

--- app/backend/src/models/users.model.js
+++ app/backend/src/models/users.model.js
@@ -1,11 +1,5 @@
 const connection = require('./connection');
 
-const findAllUser = async () => {
-  const [users] = await connection.execute(
-    'SELECT * FROM db.users',
-  );
-  return users;
-};
 
 const findByEmail = async (email) => {
   const [user] = await connection.execute(
@@ -16,6 +10,5 @@ const findByEmail = async (email) => {
 };
 
 module.exports = {
-  findAllUser,
   findByEmail,
 };
