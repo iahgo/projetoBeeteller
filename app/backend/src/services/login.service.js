c014a2ba27a24505036dd87e662db107b7fd0b4e
iahgo <iahgo@icloud.com>
2023-03-01 04:18:26 -0300

    fix: fixed doing login sucessfully

--- app/backend/src/services/login.service.js
+++ app/backend/src/services/login.service.js
@@ -3,13 +3,16 @@ const { createToken } = require('../auth/jwtFunctions');
 
 const login = async (email, password) => {
   const user = await userModel.findByEmail(email);
-  console.log(user);
+  console.log(password);
+  console.log('usuariooooo');
+  console.log(user[0]);
   if (!user.length) return { type: 'NOT FOUND', message: 'Not found' };
   const { senha, ...userWithoutPassword } = user[0];
-  console.log(userWithoutPassword);
-  const token = createToken(userWithoutPassword);
-
-  return { token };
+  if (senha === password) {
+    const token = createToken(userWithoutPassword);
+    return { token };
+  }
+  return { type: 'Password Wrong ', message: 'Email and password does not macth' };
 };
 
 module.exports = {
