d29045e5cef8caf210b2b3f03a607f19091b0789
iahgo <iahgo@icloud.com>
2023-02-28 22:58:24 -0300

    fix: values destructuring

--- app/backend/src/services/coins.service.js
+++ app/backend/src/services/coins.service.js
@@ -2,9 +2,9 @@ const coinsModel = require('../models/coins.model');
 
 const findAllValues = async (coin) => {
   const values = await coinsModel.findAllValues(coin);
-  console.log('values');
-  console.log(values);
-  const { USDBRL: { varBid, ask, create_date, ...valores } } = values;
+  const [coinName, coinValues] = Object.entries(values)[0];
+  // { USDBRL: {code: 'USD',codein: 'BRL',low: '5.2369', etc } }
+  const { varBid, ask, create_date, ...valores } = coinValues;  
   console.log('valores');
   console.log(valores);
   return { type: null, message: valores };
