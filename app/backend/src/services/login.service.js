19addebabfc0c08e48b83bc2c4ccd8ecfc42d915
iahgo <iahgo@icloud.com>
2023-02-28 22:07:19 -0300

    fix: remove password from jwttoken

--- app/backend/src/services/login.service.js
+++ app/backend/src/services/login.service.js
@@ -3,9 +3,10 @@ const { createToken } = require('../auth/jwtFunctions');
 
 const login = async (email, password) => {
   const user = await userModel.findByEmail(email);
-
+  console.log(user);
   if (!user.length) return { type: 'NOT FOUND', message: 'Not found' };
-  const { password: _password, ...userWithoutPassword } = user;
+  const { senha, ...userWithoutPassword } = user[0];
+  console.log(userWithoutPassword);
   const token = createToken(userWithoutPassword);
 
   return { token };
