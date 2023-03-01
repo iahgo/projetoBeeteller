2229bfcd153e2ecd45b21fac61d26a9a65c084e4
iahgo <iahgo@icloud.com>
2023-03-01 04:20:18 -0300

    chore: remove console logs

--- app/backend/src/services/coins.service.js
+++ app/backend/src/services/coins.service.js
@@ -2,11 +2,10 @@ const coinsModel = require('../models/coins.model');
 
 const findAllValues = async (coin) => {
   const values = await coinsModel.findAllValues(coin);
-  const [coinName, coinValues] = Object.entries(values)[0];
+  const [_coinName, coinValues] = Object.entries(values)[0];
   // { USDBRL: {code: 'USD',codein: 'BRL',low: '5.2369', etc } }
   const { varBid, ask, create_date, ...valores } = coinValues;  
-  console.log('valores');
-  console.log(valores);
+
   return { type: null, message: valores };
 };
 
