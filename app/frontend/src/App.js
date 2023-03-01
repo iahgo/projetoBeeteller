2c1e00aebc765a15d83a421135f3544830f08919
iahgo <iahgo@icloud.com>
2023-03-01 03:45:01 -0300

    chore: install react-router-dom

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,13 +1,21 @@
-// import Login from './pages/login'
-import Cotacoes from './pages/cotações'
+import Login from './pages/login'
+// import Cotacoes from './pages/cotações'
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Cotacoes from './pages/cotações';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/cotacoes" element={<Cotacoes />} />
+      </Routes>
+    {/* <div className="App"> */}
       {/* <Login /> */}
-      <Cotacoes />
-    </div>
+      {/* <Cotacoes /> */}
+    {/* </div> */}
+    </BrowserRouter>
   );
 }
 
