0a75aa3ae91465751d81bf3b69abb9ce8d641cc6
iahgo <iahgo@icloud.com>
2023-03-02 16:32:49 -0300

    fix fix cotacoes page

--- app/frontend/src/pages/cotações.jsx
+++ app/frontend/src/pages/cotações.jsx
@@ -6,14 +6,23 @@ function Cotacoes() {
     return (
     <div className="App">
       <Header />
-      <section className='dashboard'>
+
+      <section className='moedas'>
+        <div className="tituloMoedas">Moedas</div>
         <section className='coins-container'>
-          <Coins pair="USD-BRLT" imagem="0"/>
-          <Coins pair="BTC-EUR" imagem="1"/>
-          <Coins pair="BTC-USD" imagem="1"/>
+        {/* <section className='containerCardsMoedas'> */}
+          {/* <div className="cardsMoedas"> */}
+            <Coins pair="USD-BRL" imagem="0"/>
+            <Coins pair="BTC-EUR" imagem="1"/>
+            <Coins pair="BTC-USD" imagem="1"/>
+          {/* </div> */}
         </section>
       </section>
+
+        <div className="table-container">
           <Table />
+        </div>
+
     </div>
   );
 }
