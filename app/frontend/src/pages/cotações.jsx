c896cb3967cc8b9a07f1958d976b1115384c83e5
iahgo <iahgo@icloud.com>
2023-03-02 01:57:21 -0300

    refactor: refactor cotacoes

--- app/frontend/src/pages/cotações.jsx
+++ app/frontend/src/pages/cotações.jsx
@@ -8,12 +8,12 @@ function Cotacoes() {
       <Header />
       <section className='dashboard'>
         <section className='coins-container'>
-          <Coins pair="USD-BRLT" />
-          <Coins pair="BTC-EUR" />
-          <Coins pair="BTC-USD" />
-          <Table />
+          <Coins pair="USD-BRLT" imagem="0"/>
+          <Coins pair="BTC-EUR" imagem="1"/>
+          <Coins pair="BTC-USD" imagem="1"/>
         </section>
       </section>
+          <Table />
     </div>
   );
 }
