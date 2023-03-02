897210ac0b738aab29c46ca56967d85ad45abe97
iahgo <iahgo@icloud.com>
2023-03-01 21:12:59 -0300

    chore: update seed.sql

--- app/backend/seed.sql
+++ app/backend/seed.sql
@@ -3,4 +3,4 @@ USE `db`;
 INSERT INTO db.users (email, senha) VALUES
 ("iahgo@icloud.com", "abc123"),
 ("caiovidal@gmail.com", "senha123"),
-("beeteller@beeteller.com", "Bailey");
+("beeteller@beeteller.com", "beeteller");
